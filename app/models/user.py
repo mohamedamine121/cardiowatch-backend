32c38118f20837e1f27cc1cf5e87a153859b1d0b
Mohamed Amine <saidanimohamedamine82@gmail.com>
2026-04-19 22:34:01 +0100

    Fix: add photo_url in TokenResponse

--- app/models/user.py
+++ app/models/user.py
@@ -15,28 +15,29 @@ class PatientRegister(BaseModel):
     email      : str
     password   : str
     age        : int
-    medecin_id : str  # identifiant du médecin ex: MED-2024-001
+    medecin_id : str
 
 # ── Modèle Login ──────────────────────────────────
 class LoginData(BaseModel):
-    email    : str
-    password : str
-    role     : str  # "patient" ou "medecin"
-    medecin_id : Optional[str] = None  # seulement pour patient
+    email      : str
+    password   : str
+    role       : str
+    medecin_id : Optional[str] = None
 
 # ── Modèle Réponse Token ──────────────────────────
 class TokenResponse(BaseModel):
-    access_token          : str
-    role                  : str
-    nom                   : str
-    email                 : str
-    medecinNom            : Optional[str]  = None
-    medecinDisponibilite  : Optional[dict] = None
-    age                   : Optional[int]  = None
-    patient_id            : Optional[str]  = None
-    telephone             : Optional[str]  = None
-    groupe_sanguin        : Optional[str]  = None
-    poids                 : Optional[str]  = None
-    taille                : Optional[str]  = None
-    identifiant           : Optional[str]  = None
-    medecin_id            : Optional[str]  = None
+    access_token         : str
+    role                 : str
+    nom                  : str
+    email                : str
+    medecinNom           : Optional[str]  = None
+    medecinDisponibilite : Optional[dict] = None
+    age                  : Optional[int]  = None
+    patient_id           : Optional[str]  = None
+    telephone            : Optional[str]  = None
+    groupe_sanguin       : Optional[str]  = None
+    poids                : Optional[str]  = None
+    taille               : Optional[str]  = None
+    identifiant          : Optional[str]  = None
+    medecin_id           : Optional[str]  = None
+    photo_url            : Optional[str]  = None  # ✅ AJOUTER
